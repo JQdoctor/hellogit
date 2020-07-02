d26b2c31ac23d1c4ed10bc3cc351281afcd16a67
user <573887452@qq.com>
2020-07-02 14:49:11 +0800

    项目优化

--- HelloGit/src/main/java/com/atguigu/git/HelloGit.java
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -9,6 +9,7 @@ public class HelloGit {
         System.out.println("分支添加");
         System.out.println("在线更新");
         System.out.println("克隆下来的项目");
+        System.out.println("项目优化");
 
     }
 }
